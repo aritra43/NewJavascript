d2dd6773f847d9baaf2fc847c5a7f03143307a39
aritra43 <146842211+aritra43@users.noreply.github.com>
2024-06-27 11:38:43 +0000

    conatainer config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1223345
+let accountEmail = "hello"
+var accountPassword = "1234"
+accountName = "Aritra"
+
+// accountId = 1
+accountEmail = "bye"
+accountPassword = "12"
+accountName = "Aditi"
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+console.table([accountId,accountEmail,accountPassword,accountName]);
