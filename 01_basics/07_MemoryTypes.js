7d9e2b05b70e3fe3128e3698df2f2030cb546a27
aritra43 <146842211+aritra43@users.noreply.github.com>
2024-06-28 16:26:27 +0000

    container config done

--- 01_basics/07_MemoryTypes.js
+++ 01_basics/07_MemoryTypes.js
@@ -2,9 +2,9 @@
 //Stack-This is the memory utilised by the primitive data types
 //Heap-This is the memory that is used mainly by the Reference Data Type(Non-Primitive Data Type)
 //Example Of Stack Memory--
-let real=123;  //So,real has 123 in it
-let copy = real;  //So,copy has 123 in it now
-let copy=123456;  //So,copy has 123456 now
+//let real=123;  //So,real has 123 in it
+//let copy = real;  //So,copy has 123 in it now
+//let copy=123456;  //So,copy has 123456 now
 //But still now real  has 123 in it .
 
 
@@ -13,8 +13,15 @@ let copy=123456;  //So,copy has 123456 now
 
 //Example Of Heap Memory--
 let real= {
-    numOne:50
-    numTwo:100
+    numOne : 50
 }
 let copy = real;
+copy.numOne = 150
 console.log(copy.numOne);
+console.log(real.numOne);
+console.log(real.numTwo);
+
+//In this case as object is used therefore it is a Reference Data Type(Non-Primitive Data Type) .Therefore,it mainly utilises the heap ememory .So,when the real object is stored in another variable then instead of passsing a copy of the real object it mainly passes the original reference to the copy variable.So,any changes brought about in the copy variable brings about a change in the original content and hence as the reference point of the both variables in case of the heap memory is same therefore it also brings about a change in the real value on changing the value of the copy variable.
+
+//This in case of the stack memeory will not happen as when the real avriable is stored in the copy variable then it mainly passes a copy of the real variable so any change in the copy variable will not be reflected on the value of the real variable .
+
