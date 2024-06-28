736e9343500f6b5a6f1e4b1f75b398eeb1e8289f
aritra43 <146842211+aritra43@users.noreply.github.com>
2024-06-28 11:03:44 +0000

    container config done

--- 01_basics/06_DataTypesDetails.js
+++ 01_basics/06_DataTypesDetails.js
@@ -4,3 +4,10 @@
 
 //Is JavaScript a Dynamically Typed or a Statically Typed Language?
 //Javascript is a Dynamically Typed Language as for the values to be stored we do not need to define the data type of the values that is stored in the given variable .Instead ,JavaScript is such a language that it itself determines the datatype of the value given to a variable during the runtime of the code.
+
+//Data Types of -
+//Arrays-Objects
+//Objects-Objects
+//Functions-Function(Function Objects)
+//Null-Objects
+//Undefined-Undefined
