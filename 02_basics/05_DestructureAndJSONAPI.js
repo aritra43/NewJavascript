d18344c19ffa2eea077eb8a9a276572ced478e8f
aritra43 <146842211+aritra43@users.noreply.github.com>
2024-07-01 11:54:31 +0000

    container config done

--- 02_basics/05_DestructureAndJSONAPI.js
+++ 02_basics/05_DestructureAndJSONAPI.js
@@ -17,7 +17,7 @@ console.log(newEmail);    //Output is Aritra@google.com
 //API-It mainly serves two purposes ----
 //One is that it mainly helps in communication of two or more different languages in a given application
 //And the second is that it can Collect or Give Data by itself without leting the user know the complexities of the process.(That is iyt promotes the process of Data Abstraction).
-//So,when the backend mainly transsmits a data then through this API the details will be send in the form of the JSON Objects.
+//So,when the backend mainly transmits a data then through this API the details will be send in the form of the JSON(JavaScript Object Notation) Objects.
 //JSON is also an Object that has no Object name
 {
     name:"Aritra",
